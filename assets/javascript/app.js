3f2da48fcfeb360727d4e4f08a6adb133dad32da
Dennis Ruto <rutodeno@gmail.com>
2018-04-08 23:43:22 -0400

    added a restart btn

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,14 +8,14 @@
 // After five questions, show the results
 
 
-
 var timer = 30;
-var intervalTimer =0;
-var invervalPrompt=0; // hold 
+var intervalTimer = 0;
+var intervalPrompt = 0; // hold 
 var timeoutDisplay = 0;
 var count = 0;
 var timerChecker = false;
 var correctAnswer = 0;
+var restartBtn;
 
 var trivia = [{
     q: "What does the 'AA' arm patch, worn by the 82nd soldiers, stand for ?",
@@ -60,11 +60,10 @@ var trivia = [{
     q: "Which 1962 classic movie featured the 82d Airborne Division  ?",
     a: ["a. Frozen",
         "b. Dunkirk",
-        "c. Airbone",	
+        "c. Airbone",
         "d. The Longest Day"]
 }
 ];
-
 
 
 $(document).ready(function () {
@@ -81,118 +80,147 @@ $(document).ready(function () {
 });
 
 
-$(document).on('click', '.answer', function() {
+$(document).on('click', '.answer', function () {
 
 
     console.log($(this).text());
     console.log(trivia[count].a[0]);
-    console.log("count: " +count);
+    console.log("count: " + count);
+
+    
+    if (!timerChecker) {    // every time you click an answer, the clock resets
+        timer = 30;
+    };
 
     // this is where the magic happens
     // checking user input. if user selects an answer, check it and see if its correct. grade the use at the end
 
+    if ((count === 0) && ($(this).text() === trivia[count].a[0])) {
 
-       if ( (count === 0) && ($(this).text() === trivia[count].a[0]) ){
-            
-            console.log("you got it");
-		    correctAnswer ++;
+        console.log("you got it");
+        correctAnswer++;
 
-        } else if ( (count === 1) && ($(this).text() === trivia[count].a[3]) ){
-            
-            console.log("you got it");
-		    correctAnswer ++;
-        } else if ( (count === 2) && ($(this).text() === trivia[count].a[2]) ){
-            
-            console.log("you got it");
-        } else if ( (count === 3) && ($(this).text() === trivia[count].a[2]) ){
-            
-            console.log("you got it");
-		    correctAnswer ++;
-        } else if ( (count === 4) && ($(this).text() === trivia[count].a[0]) ){
-            
-            console.log("you got it");
-		    correctAnswer ++;
-        } else if ( (count === 5) && ($(this).text() === trivia[count].a[0]) ){
-            
-            console.log("you got it");
-		    correctAnswer ++;
-        } else {
+    } else if ((count === 1) && ($(this).text() === trivia[count].a[3])) {
 
-            console.log("you didn't get it");
-        }
+        console.log("you got it");
+        correctAnswer++;
+    } else if ((count === 2) && ($(this).text() === trivia[count].a[2])) {
 
-        if (count === (trivia.length -1)) {
+        console.log("you got it");
+    } else if ((count === 3) && ($(this).text() === trivia[count].a[2])) {
 
-            stopTrivia();
-        };
+        console.log("you got it");
+        correctAnswer++;
+    } else if ((count === 4) && ($(this).text() === trivia[count].a[0])) {
 
-  
-        loadTrivia()
+        console.log("you got it");
+        correctAnswer++;
+    } else if ((count === 5) && ($(this).text() === trivia[count].a[0])) {
+
+        console.log("you got it");
+        correctAnswer++;
+    } else {
+
+        console.log("you didn't get it");
+    }
+
+    loadNextQuestion()
+    
+
+    if (count > 5) {     // stop the game.  Enable restart functionality.
+
+        stopTrivia();
+    };
 
 
 
 });
 
 
-$(document).on("mouseenter",".answer",function () {
+$(document).on("mouseenter", ".answer", function () {
     // why does 'this' work and not .answer ?
-    $(this).css("background-color", "#73a4c9");  
-   
+    $(this).css("background-color", "#73a4c9");
+
 });
 
-$(document).on("mouseleave",".answer",function () {
+$(document).on("mouseleave", ".answer", function () {
 
     $(this).css("background-color", "#fff");
-   
+
 });
 
-function displayTrivia() {
+$(document).on("click", "#restartID", function(){
+    console.log("yo");
+    restartGame();
 
-    $("#wrapper").html(trivia[count].q + "<br>" +
-        "<div class ='answer'>" + trivia[count].a[0] + "</div><br>" +
-        "<div class ='answer'>" + trivia[count].a[1] + "</div><br>" +
-        "<div class ='answer'>" + trivia[count].a[2] + "</div><br>" +
-        "<div class ='answer'>" + trivia[count].a[3] + "</div>"
-    );
+});
+
+function displayQuestion() {
+
+    if (count < 6 ) {
+        $("#wrapper").html(trivia[count].q + "<br>" +
+            "<div class ='answer'>" + trivia[count].a[0] + "</div><br>" +  // update time on DOM
+            "<div class ='answer'>" + trivia[count].a[1] + "</div><br>" +
+            "<div class ='answer'>" + trivia[count].a[2] + "</div><br>" +
+            "<div class ='answer'>" + trivia[count].a[3] + "</div>"
+        );
+
+    }
 
 };
 
-function displayTime() {
-    intervalTimer = setInterval(timeforPrompt, 1000)
-}
-
 function timeforPrompt() {
     timer--;
-    $("#timeDisplay").html("<p>Time Remaining: " + timer + "</p>");
+    $("#timeDisplay").html("<p>Time Remaining: " + timer + "</p>"); // update time on DOM
 
     if (timer === 0) {
         timer = 30; // restarting timer
     };
-
 }
 
-
-function loadTrivia() {
+function loadNextQuestion() {
 
     count++;
     console.log("count: " + count)
+    var timeoutDisplay = setTimeout(displayQuestion, 1000); // loading our questions
+    console.log(timeoutDisplay + " TimeoutDisplay");
 
-    var timeoutDisplay = setTimeout(displayTrivia, 1000);
-    console.log(timeoutDisplay +"TimeoutDisplay");
 
 };
 
+
+function displayTime() {
+    
+    intervalTimer = setInterval(timeforPrompt, 1000) // loading our timer
+    intervalPrompt = setInterval(loadNextQuestion, timer*1000); // loading our questions every 30 seconds
+    console.log("Trivia Timeout "+intervalPrompt);
+    console.log("TImer Prompt "+intervalTimer);
+}
+
+
 function startTrivia() {
     displayTime();
-    displayTrivia();
-    intervalPrompt = setInterval(loadTrivia, 30000);
-    console.log("showPrompt"+intervalPrompt);
+    displayQuestion();
 
 };
 
 function stopTrivia() {
-    clearInterval(invervalPrompt);
+    clearInterval(intervalPrompt);
     clearInterval(intervalTimer);
     clearTimeout(timeoutDisplay);
+
+    $("#wrapper").html("Congratulation !!! You have completed the trivia. Click <strong> Restart </strong> to start the game again.");
+
+    var restart = $("<button type ='button' id='restartID' class='btn btn-info btn-lg'>Restart</button>");
+
+    $("#restartBtn").append(restart);
+
+};
+
+function restartGame() { // restart game. 
+
+    count = 0;
+    timer = 30;
+    startTrivia();
 
 };
